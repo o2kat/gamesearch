4138e7bd651bcdaeb0006f84046ec90ca7f00d6b
azat <azatmuham8@gmail.com>
2025-03-22 22:00:38 +0300

    Возвращение старого main.tsx с рабочим функционалом

--- mygameapp/src/main.tsx
+++ mygameapp/src/main.tsx
@@ -1,10 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App';
-import GameDetail from './GameDetail';
-import Favorites from './Favorites';
+import './index.css';
+import App from './App.tsx';
+import GameDetail from './GameDetail.tsx';
+import Favorites from './Favorites.tsx';
 
-function Main() {
-    return (
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
         <Router>
             <Routes>
                 <Route path="/" element={<App />} />
@@ -12,7 +15,5 @@ function Main() {
                 <Route path="/favorites" element={<Favorites />} />
             </Routes>
         </Router>
-    );
-}
-
-export default Main;
+    </StrictMode>,
+);
